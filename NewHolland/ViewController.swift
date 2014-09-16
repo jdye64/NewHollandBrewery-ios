e437e0ffb5807967ff0fafa196edcb312ce6b17d
Jeremy Dyer <jdye64@gmail.com>
2014-09-16 14:47:56 -0400

    Initial Commit

---
+++ NewHolland/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewHolland
+//
+//  Created by Jeremy Dyer on 9/16/14.
+//  Copyright (c) 2014 Jeremy Dyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
